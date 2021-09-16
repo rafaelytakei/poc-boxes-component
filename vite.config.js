ab5bdf49c5431dc5a8ebe8a03ab5fedef21ba3b8
Rafael Takei <60942829+rafaelytakei@users.noreply.github.com>
2021-09-16 19:48:40 -0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+import Components from 'unplugin-vue-components/vite'
+import * as path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import WindiCSS from 'vite-plugin-windicss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    Vue(),
+    Pages(),
+    Layouts(),
+    Components({
+      dts: true,
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+    }),
+    WindiCSS(),
+  ],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src') },
+      { find: '~', replacement: path.resolve(__dirname, './src') },
+    ],
+  },
+  server: {
+    fs: {
+      allow: ['.'],
+    },
+  },
+})
